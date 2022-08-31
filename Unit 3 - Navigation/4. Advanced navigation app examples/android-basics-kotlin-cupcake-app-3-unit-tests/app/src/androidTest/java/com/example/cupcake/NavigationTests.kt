236ddeebd4c4f88c6fd48d2f4386ed49a7d293fc
Andrei <carolsfartz@gmail.com>
2022-08-31 18:02:04 +0300

    add test for navigating between the 4 Fragments

--- Unit 3 - Navigation/4. Advanced navigation app examples/android-basics-kotlin-cupcake-app-3-unit-tests/app/src/androidTest/java/com/example/cupcake/NavigationTests.kt
+++ Unit 3 - Navigation/4. Advanced navigation app examples/android-basics-kotlin-cupcake-app-3-unit-tests/app/src/androidTest/java/com/example/cupcake/NavigationTests.kt
@@ -17,18 +17,75 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class NavigationTests {
 
+    // NOTE: Unlike a NavHostController instance that a NavHostFragment would use,
+    // TestNavHostController does not trigger the underlying navigate() behavior
+    // (such as the FragmentTransaction that FragmentNavigator does) when you call navigate().
+    // It only updates the state of the TestNavHostController.
     @Test
-    fun navigation_fromStartFragment_toFlavorFragment() {
+    fun navigation_StartFragment_to_FlavorFragment() {
+        // Create a TestNavHostController
         val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        // Create a graphical FragmentScenario for the StartFragment
         val startFragmentScenario: FragmentScenario<StartFragment> = launchFragmentInContainer(themeResId = R.style.Theme_Cupcake)
 
         startFragmentScenario.onFragment { fragment ->
+            // Set the graph on the TestNavHostController
             navController.setGraph(R.navigation.nav_graph)
+            // Make the NavController available via the findNavController() APIs
             Navigation.setViewNavController(fragment.requireView(), navController)
         }
 
+        // Verify that performing a click changes the NavController’s state
         onView(withId(R.id.order_one_cupcake)).perform(click())
         assertEquals(navController.currentDestination?.id, R.id.flavorFragment)
+    }
+
+    @Test
+    fun navigation_FlavorFragment_to_PickupFragment() {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        val flavorFragmentScenario: FragmentScenario<FlavorFragment> = launchFragmentInContainer(themeResId = R.style.Theme_Cupcake)
+
+        flavorFragmentScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            // Set the current destination to the FlavorFragment
+            navController.setCurrentDestination(R.id.flavorFragment)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(withId(R.id.next_button)).perform(click())
+        assertEquals(navController.currentDestination?.id, R.id.pickupFragment)
+    }
+
+    @Test
+    fun navigation_PickupFragment_to_SummaryFragment() {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        val flavorFragmentScenario: FragmentScenario<PickupFragment> = launchFragmentInContainer(themeResId = R.style.Theme_Cupcake)
+
+        flavorFragmentScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            // Set the current destination to the PickupFragment
+            navController.setCurrentDestination(R.id.pickupFragment)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(withId(R.id.next_button)).perform(click())
+        assertEquals(navController.currentDestination?.id, R.id.summaryFragment)
+    }
+
+    @Test
+    fun navigation_SummaryFragment_to_StartFragment() {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        val flavorFragmentScenario: FragmentScenario<SummaryFragment> = launchFragmentInContainer(themeResId = R.style.Theme_Cupcake)
+
+        flavorFragmentScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            // Set the current destination to the StartFragment
+            navController.setCurrentDestination(R.id.summaryFragment)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(withId(R.id.cancel_button)).perform(click())
+        assertEquals(navController.currentDestination?.id, R.id.startFragment)
     }
 
 }
