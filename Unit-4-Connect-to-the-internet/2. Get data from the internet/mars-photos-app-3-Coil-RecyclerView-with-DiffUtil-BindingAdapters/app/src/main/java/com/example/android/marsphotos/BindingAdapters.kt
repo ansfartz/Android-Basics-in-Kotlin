ed04aafc44993f8b601fe2c64b7677b637761b68
Andrei Sfartz <carolsfartz@gmail.com>
2022-10-08 21:25:51 +0300

    edit comment explaining Data Binding Adapters

--- Unit-4-Connect-to-the-internet/2. Get data from the internet/mars-photos-app-3-Coil-RecyclerView-with-DiffUtil-BindingAdapters/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ Unit-4-Connect-to-the-internet/2. Get data from the internet/mars-photos-app-3-Coil-RecyclerView-with-DiffUtil-BindingAdapters/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -14,44 +14,45 @@ import com.example.android.marsphotos.overview.PhotoGridAdapter
 /**
  * https://developer.android.com/topic/libraries/data-binding/binding-adapters
  *
- * Binding Adapters are annotated methods used to create custom setters for custom properties of your view.
+ * Binding Adapters are annotated methods used to create custom setters for custom properties of
+ * your view.
  *
- * Usually when you set an attribute in your XML using the code: android:text="Sample Text",
- * the Android system automatically looks for a setter with the same name as the text attribute,
- * which is set by the setText(String: text) method. The setText(String: text) method is a setter method
- * for some views provided by the Android Framework.
+ * Usually when you set an attribute in your XML using the code: android:text="Sample Text", the
+ * Android system automatically looks for a setter with the same name as the 'text' attribute,
+ * which is set by the setText(String: text) method.
+ * The setText(String: text) method is a setter method for some views provided by the Android Framework.
  * Similar behavior can be customized using the binding adapters; you can provide a custom attribute
  * and custom logic that will be called by the Data binding library.
  *
  * Example:
- *   Something more complex than simply calling a setter on the Image view.
  *   Consider loading images off the UI thread (main thread), from the internet.
- *   First, choose a custom attribute to assign the image to an ImageView. In the following example it is 'someImageUrl'.
+ *   First, choose a custom attribute to assign the image to an ImageView ---> 'myImageUrl'.
  *
- *          <ImageView
- *              android:layout_width="wrap_content"
- *              android:layout_height="wrap_content"
- *              app:someImageUrl="@{product.imageUrl}"/>
+ *       <ImageView
+ *           android:layout_width="wrap_content"
+ *           android:layout_height="wrap_content"
+ *           app:myImageUrl="@{product.imageUrl}"/>
  *
- *   If you do not add any further code, the system would look for a setSomeImageUrl(String) method
- *   on ImageView and not find it, throwing an error because this is a custom attribute not provided by the framework.
- *   You have to create a way to implement and set the app:someImageUrl attribute to the ImageView. You will use Binding adapters (annotated methods) to do this.
+ *   If you do not add any further code, the system would look for a setMyImageUrl(String) method
+ *   in ImageView.java and not find it, throwing an error because this is a custom attribute not
+ *   provided by the framework.
+ *   You have to create a way to set the 'app:myImageUrl' attribute to the ImageView. You will use
+ *   Binding adapters (annotated methods) to do this.
  *
- * Example of a Binding Adapter:
+ *       @BindingAdapter("myImageUrl")
+ *       fun bindImage(imgView: ImageView, imgUrl: String?) {
+ *           imgUrl?.let {
+ *               // Load the image in the background using Coil.
+ *               }
+ *           }
+ *       }
  *
- *          @BindingAdapter("someImageUrl")
- *          fun bindImage(imgView: ImageView, imgUrl: String?) {
- *              imgUrl?.let {
- *                  // Load the image in the background using Coil.
- *                  }
- *              }
- *          }
+ *  The @BindingAdapter() annotation takes the attribute name as its parameter.
+ *  In the bindImage(...) method, the first method parameter is the type of the target View and the
+ *  second is the value being set to the attribute.
  *
- *  The @BindingAdapter annotation takes the attribute name as its parameter.
- *  In the bindImage(...) method, the first method parameter is the type of the target View and the second is the value being set to the attribute.
- *
- *  Basically, for the above example, whenever the attribute 'someImageUrl' is being used, the method bindImage(...) will be called.
- *  See more examples in the docs: https://developer.android.com/topic/libraries/data-binding/binding-adapters
+ *  Basically, for the above example, whenever the attribute 'myImageUrl' is being used, the method
+ *  annotated with @BindingAdapter("myImageUrl") will be called
  *
  */
 @BindingAdapter("imageUrl")
---
+++ Unit-5-Data-persistence/2. Room for Data persistence/inventory-3-Data-Binding/app/src/main/java/com/example/inventory/ui/BindingAdapters.kt
@@ -0,0 +1,4 @@
+package com.example.inventory.ui
+
+class BindingAdapters {
+}
